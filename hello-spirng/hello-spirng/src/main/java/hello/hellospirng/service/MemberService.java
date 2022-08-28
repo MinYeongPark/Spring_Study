8603ee8a729a4864ec70d81e09c91f07403111e2
MinYeongPark <yingo24655@naver.com>
2022-08-28 22:55:53 +0900

    [ADD] 섹션4-1) 컴포넌트 스캠과 자동 의존관계 설정

--- hello-spirng/hello-spirng/src/main/java/hello/hellospirng/service/MemberService.java
+++ hello-spirng/hello-spirng/src/main/java/hello/hellospirng/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospirng.service;
 import hello.hellospirng.domain.Member;
 import hello.hellospirng.repository.MemberRepository;
 import hello.hellospirng.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) { // 외부에서 멤버리포지토리 넣어지도록 함
         this.memberRepository = memberRepository;
     }
