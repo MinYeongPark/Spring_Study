50981e27308321b6b9345b6ee66111186ab059ed
MinYeongPark <yingo24655@naver.com>
2023-01-30 14:56:18 +0900

    [ADD] 섹션 2~3.회원 관리 예제 - 백엔드 개발 까지 (다시 진행)

---
+++ hello-spring2/src/main/java/hello/hellospring2/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hellospring2.repository;
+
+import hello.hellospring2.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // 키 값을 생성해주는 것
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); // 아이디는 시스템이 정해줌
+        store.put(member.getId(), member); // 저장
+        return member; // 저장 결과 반환
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // 감싸서 반환함
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) // 이름을 가지고 비교해서 찾음.
+                .findAny(); // 하나라도 찾음.
+        // 루프를 다 돌면서 하나라도 찾으면 반환하고, 하나도 없으면 Optional에 넣어서 반환함.
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // 멤버들이 반환됨.
+    }
+
+    public void clearStore() {
+        store.clear(); // 스토어를 싹 비움
+    }
+}
---
+++ hello-spring2/src/main/java/hello/hellospring2/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.hellospring2.service;
+
+import hello.hellospring2.domain.Member;
+import hello.hellospring2.repository.MemberRepository;
+import hello.hellospring2.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) { // DI 방법
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) // 널일 가능성이 있으면 Optional로 감쌈.
+                .ifPresent(m -> { // 값이 있으면
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
