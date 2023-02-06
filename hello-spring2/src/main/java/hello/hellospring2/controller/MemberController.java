4b7a16f7eb83439efbc2615b2aca97e5e274a563
MinYeongPark <yingo24655@naver.com>
2023-02-06 13:52:44 +0900

    [ADD] 섹션4-2. 자바 코드로 직접 스프링 빈 등록하기 (다시 진행)

--- hello-spring2/src/main/java/hello/hellospring2/controller/MemberController.java
+++ hello-spring2/src/main/java/hello/hellospring2/controller/MemberController.java
@@ -6,12 +6,19 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+//    @Autowired private final MemberService memberService; // 필드 주입 방법 (별로 안 좋음)
 
-    @Autowired
+    private MemberService memberService;
+
+    @Autowired // 생성자 주입 (가장 좋은 방법)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+//    @Autowired // Setter 주입 -> 누군가가 호출할 때 public으로 노출되어야 함. 중간에 잘못 바꾸면 문제가 될 수 있음.
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 }
 
 
