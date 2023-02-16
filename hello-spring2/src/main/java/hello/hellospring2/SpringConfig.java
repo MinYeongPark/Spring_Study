8bc51852b45a4f8590b3a9418883f76d076a7d71
MinYeongPark <yingo24655@naver.com>
2023-02-16 23:43:23 +0900

    [ADD] ~섹션6-4.스프링 JdbcTemplate

--- hello-spring2/src/main/java/hello/hellospring2/SpringConfig.java
+++ hello-spring2/src/main/java/hello/hellospring2/SpringConfig.java
@@ -1,13 +1,26 @@
 package hello.hellospring2;
 
+import hello.hellospring2.repository.JdbcMemberRepository;
+import hello.hellospring2.repository.JdbcTemplateMemberRepository;
 import hello.hellospring2.repository.MemberRepository;
 import hello.hellospring2.repository.MemoryMemberRepository;
 import hello.hellospring2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource; // 스프링이 데이터베이스와 연결할 수 있는 정보가 있는 것을 만들어주고 주입해줌.
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource); // 이 조립하는 부분만 수정하면 딱 연결되는 게 스프링의 장점.
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
---
+++ hello-spring2/src/test/java/hello/hellospring2/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,53 @@
+package hello.hellospring2.service;
+
+import hello.hellospring2.domain.Member;
+import hello.hellospring2.repository.MemberRepository;
+import hello.hellospring2.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void duplicate_member_exception() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));// 이 로직을 실행시키기 위해 IllegalStateException.class 예외가 발생해야 함.
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+}
