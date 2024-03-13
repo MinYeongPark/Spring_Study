f2192207be802c6541388366534d6c207b655b10
Min Yeong Park <yingo24655@naver.com>
2024-03-13 11:44:58 +0900

    [ADD] [FC] 6-2. (3) Step - 기본 적용 - tasklet으로 실행

--- fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
+++ fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
@@ -3,9 +3,12 @@ package com.fastcampus.batchcampus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,11 +27,26 @@ public class JobConfiguration {
 
     @Bean
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("step", jobRepository)
-                .tasklet((a, b) -> {
-                    log.info("step");
+        final Tasklet tasklet = new Tasklet() {
+
+            private int count = 0;
+
+            @Override
+            public RepeatStatus execute(StepContribution a, ChunkContext b) throws Exception {
+                count++;
+
+                if (count == 15) {
+                    log.info("Tasklet FINISHED");
                     return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
+                }
+
+                log.info("Tasklet CONTINUABLE {}", count);
+                return RepeatStatus.CONTINUABLE;
+            }
+        };
+
+        return new StepBuilder("step", jobRepository)
+                .tasklet(tasklet, platformTransactionManager)
                 .build();
     }
 }
