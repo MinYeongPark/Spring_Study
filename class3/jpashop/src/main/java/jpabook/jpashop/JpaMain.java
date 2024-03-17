f3ee606d8b8b17315248c57199010cf863c58e13
Min Yeong Park <yingo24655@naver.com>
2024-03-17 21:51:32 +0900

    [ADD] [ORM 표준 JPA] ~섹션5-4. 실전 예제 2 - 연관관계 매핑 시작

--- class3/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ class3/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -3,6 +3,7 @@ package jpabook.jpashop;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 import java.util.List;
 
@@ -18,8 +19,13 @@ public class JpaMain {
 
         try {
 
-            Order order = em.find(Order.class, 1L);
-            Member member = order.getMember();
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
 
             tx.commit(); // 트랜잭션 커밋 시 아무런 일도 일어나지 않게 됨.
         } catch (Exception e) {
