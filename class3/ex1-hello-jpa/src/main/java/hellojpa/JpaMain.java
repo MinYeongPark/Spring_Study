25387f7ffb9e869dea4a398ebdbdc55bc24de632
Min Yeong Park <yingo24655@naver.com>
2024-03-17 20:15:55 +0900

    [ADD] [ORM 표준 JPA] ~섹션5-1. 단방향 연관관계

--- class3/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ class3/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,23 @@ public class JpaMain {
         tx.begin(); // DB 트랜잭션 시작
 
         try {
+
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
             Member member = new Member();
-            member.setUsername("C");
-
-            System.out.println("===================");
-
+            member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
 
-            System.out.println("member.id = " + member.getId()  );
-            System.out.println("===================");
+            em.flush(); // 현재 영속성 컨텍스트에 있는 DB에 쿼리를 다 날려서
+            em.clear(); // 영속성 컨텍스트 초기화
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
             tx.commit(); // 트랜잭션 커밋 시 아무런 일도 일어나지 않게 됨.
         } catch (Exception e) {
--- class3/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ class3/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -7,21 +7,20 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", insertable = true, updatable = true)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member() {
-    }
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -37,5 +36,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
