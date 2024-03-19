bb12753b4069b136509d9dce36bdab23b6af4d42
Min Yeong Park <yingo24655@naver.com>
2024-03-20 08:13:15 +0900

    [ADD] [FC] 6-2. (3) Step - 기본 적용 - chunk 기반

--- fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
+++ fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -11,11 +12,9 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.skip.SkipLimitExceededException;
 import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -32,31 +31,57 @@ public class JobConfiguration {
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-        ItemReader<Integer> itemReader = new ItemReader<>() {
-            private int count = 0;
-
-            @Override
-            public Integer read() {
-                count++;
-
-                log.info("Read {}", count);
-
-                if (count >= 15) {
-                    throw new IllegalStateException("예외가 발생했어요");
-                }
-
-                return count;
-            }
-        };
-
+    public Step step(
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager,
+            @Value("#{jobParameters['name']}") String name
+    ) {
+        log.info("name : {}" ,name);
         return new StepBuilder("step", jobRepository)
-                .chunk(10, platformTransactionManager)
-                .reader(itemReader)
-//                .processor()
-                .writer(read -> {})
-                .faultTolerant()
-                .skipPolicy((t, skipCount) -> t instanceof IllegalStateException && skipCount < 5)
+                .tasklet((a, b) -> RepeatStatus.FINISHED, platformTransactionManager)
                 .build();
     }
+
+//    @Bean
+//    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+//        ItemReader<Integer> itemReader = new ItemReader<>() {
+//            private int count = 0;
+//
+//            @Override
+//            public Integer read() {
+//                count++;
+//
+//                log.info("Read {}", count);
+//
+//                if (count == 20)
+//                    return null;
+//
+////                if (count >= 15) {
+////                    throw new IllegalStateException("예외가 발생했어요");
+////                }
+//
+//                return count;
+//            }
+//        };
+//
+//        ItemProcessor<Integer, Integer> itemProcessor = new ItemProcessor<>() {
+//            @Override
+//            public Integer process(Integer item) throws Exception {
+//                if (item == 15)
+//                    throw new IllegalStateException();
+//
+//                return item;
+//            }
+//        };
+//
+//        return new StepBuilder("step", jobRepository)
+//                .<Integer, Integer>chunk(10, platformTransactionManager)
+//                .reader(itemReader)
+//                .processor(itemProcessor)
+//                .writer(read -> {})
+//                .faultTolerant()
+//                .retry(IllegalStateException.class)
+//                .retryLimit(5)
+//                .build();
+//    }
 }
