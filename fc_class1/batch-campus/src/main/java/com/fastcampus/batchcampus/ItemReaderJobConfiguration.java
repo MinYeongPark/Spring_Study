d7ec0e2e3cecd5648d38538dbed0495a7a8edf9f
Min Yeong Park <yingo24655@naver.com>
2024-03-20 11:35:03 +0900

    [ADD] [FC] 6-2. (4-3) ItemReader - Database

--- fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/ItemReaderJobConfiguration.java
+++ fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/ItemReaderJobConfiguration.java
@@ -1,5 +1,7 @@
 package com.fastcampus.batchcampus;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -7,6 +9,8 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.Range;
@@ -36,15 +40,16 @@ public class ItemReaderJobConfiguration {
     public Step step(
             JobRepository jobRepository,
             PlatformTransactionManager transactionManager,
-            ItemReader<User> jsonItemReader
+            ItemReader<User> jpaCursorItemReader
     ) {
         return new StepBuilder("step", jobRepository)
                 .<User, User>chunk(2, transactionManager)
-                .reader(jsonItemReader)
+                .reader(jpaCursorItemReader)
                 .writer(System.out::println)
                 .build();
     }
 
+    // delimiter로 끊어서 읽어오기
     @Bean
     public FlatFileItemReader<User> flatFileItemReader() {
         return new FlatFileItemReaderBuilder<User>()
@@ -58,6 +63,7 @@ public class ItemReaderJobConfiguration {
                 .build();
     }
 
+    // fixedLength - 고정 길이로 읽어오기
     @Bean
     public FlatFileItemReader<User> fixedLengthFlatFileItemReader() {
         return new FlatFileItemReaderBuilder<User>()
@@ -72,12 +78,38 @@ public class ItemReaderJobConfiguration {
                 .build();
     }
 
+    // json
     @Bean
     public JsonItemReader<User> jsonItemReader() {
         return new JsonItemReaderBuilder<User>()
                 .name("jsonItemReader")
                 .resource(new ClassPathResource("users.json"))
                 .jsonObjectReader(new JacksonJsonObjectReader<>(User.class))
+                .build();
+    }
+
+    // DB 읽기 - paging 기반
+    @Bean
+    public ItemReader<User> jpaPagingItemReader(
+            EntityManagerFactory entityManagerFactory
+    ) {
+        return new JpaPagingItemReaderBuilder<User>()
+                .name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(3)
+                .queryString("SELECT u FROM User u ORDER BY u.id")
+                .build();
+    }
+
+    // DB 읽기 - cursor 기반
+    @Bean
+    public ItemReader<User> jpaCursorItemReader(
+            EntityManagerFactory entityManagerFactory
+    ) {
+        return new JpaCursorItemReaderBuilder<User>()
+                .name("jpaCursorItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("SELECT u FROM User u ORDER BY u.id")
                 .build();
     }
 }
--- fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/User.java
+++ fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/User.java
@@ -1,10 +1,17 @@
 package com.fastcampus.batchcampus;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private String age;
     private String region;
