b6888eb7ed60d4a9bb38549675e26e2930d46299
Min Yeong Park <yingo24655@naver.com>
2024-03-20 10:27:14 +0900

    [ADD] [FC] 6-2. (4-1) ItemReader - File

---
+++ fc_class1/batch-campus/src/main/java/com/fastcampus/batchcampus/User.java
@@ -0,0 +1,12 @@
+package com.fastcampus.batchcampus;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private String age;
+    private String region;
+    private String telephone;
+}
