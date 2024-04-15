78354ea932f0a6af02ace69362c053ec9dec4d02
Min Yeong Park <yingo24655@naver.com>
2024-04-15 10:06:21 +0900

    [ADD] [JPA 활용2] 섹션 3. API 개발 고급 - 지연 로딩과 조회 성능 최적화 (2) 간단한 주문 조회 V2 : 엔티티를 DTO로 변환

--- class2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ class2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * [xToOne(ManyToOne, OneToOne) 관계에서 어떻게 성능최적화를 할 수 있을지 실습함]
@@ -30,5 +35,35 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // order 2개
+        // n + 1 문제 = 1 + Member N개 + Delivery N개
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch()); // n개 (2개) 가져옴
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
