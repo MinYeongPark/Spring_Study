5ce1611ced6b7f2e0e35c783ea32f68c01a78a2b
Min Yeong Park <yingo24655@naver.com>
2024-04-22 08:54:45 +0900

    [ADD] [JPA 활용2] 섹션 4. API 개발 고급 - 컬렉션 조회 최적화 (6) V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- class2/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ class2/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
@@ -15,8 +17,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,7 +51,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
     }
@@ -58,7 +66,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
     }
@@ -76,7 +84,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
     }
@@ -89,6 +97,48 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        // 이 코드에 Cannot resolve method 'getOrderId' in 'T' 오류가 나서.. 다른 분의 코드로 작업했다.
+//        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+//
+//        return flats.stream()
+//                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+//                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+//                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+//                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+//                )).entrySet().stream()
+//                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+//                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+//                        e.getKey().getAddress(), e.getValue()))
+//                .collect(toList());
+
+
+        // 출처 : https://www.inflearn.com/questions/1077515/%EC%A7%88%EB%AC%B8x-orderv6-for%EB%AC%B8%EC%9C%BC%EB%A1%9C-%EB%B0%9C%EB%9D%BC%EB%82%B4%EA%B8%B0
+        List<OrderFlatDto> orderFlats = orderQueryRepository.findAllByDto_flat();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = new HashMap<>();
+        Map<Long, OrderQueryDto> orderMap = new HashMap<>();
+
+        orderFlats.forEach(orderFlat -> { // OrderFlatDto -> OrderQueryDto 로 변환하는 코드
+            Long orderId = orderFlat.getOrderId();
+            if(orderMap.get(orderId) == null){
+                orderMap.put(orderId,new OrderQueryDto(orderId, orderFlat.getName(), orderFlat.getOrderDate(), orderFlat.getOrderStatus(), orderFlat.getAddress()));
+            }
+
+            if(orderItemMap.get(orderId) == null){
+                orderItemMap.put(orderId,new ArrayList<OrderItemQueryDto>());
+            }
+            orderItemMap.get(orderId).add(new OrderItemQueryDto(orderId, orderFlat.getItemName(), orderFlat.getOrderPrice(), orderFlat.getCount()));
+        });
+
+        orderItemMap.forEach((orderId, orderItem)->{
+            orderMap.get(orderId).setOrderItems(orderItem);
+        });
+
+        return new ArrayList<OrderQueryDto>(orderMap.values());
     }
 
     @Getter
@@ -109,7 +159,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
             orderItems = order.getOrderItems().stream()
                     .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
 
