fcd553f74363a65dbc8b18438b7db45baed88fad
Min Yeong Park <yingo24655@naver.com>
2024-07-02 21:25:46 +0900

    [ADD] [FC] 7-3. (8) Spring Webflux 실습(2) - mvc 프로젝트 생성

---
+++ fc_class3/webflux1/src/main/java/com/example/webflux1/client/PostClient.java
@@ -0,0 +1,28 @@
+package com.example.webflux1.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class PostClient {
+    private final WebClient webClient;
+    private final String url = "http://127.0.0.1:8090";
+
+    // Spring Webflux에서 WebClient를 사용해서 -> mvc 서버에 ("/posts/{id}") path로 웹 요청을 하는 코드
+    public Mono<?> getPost(Long id) {
+        String uriString = UriComponentsBuilder.fromHttpUrl(url)
+                .path("/posts/%d".formatted(id))
+                .buildAndExpand()
+                .toUriString();
+
+        webClient.get() // 메서드를 먼저 선택 -> 여기에서는 get 메서드로 받겠다고 설정
+                .uri(uriString)
+                .retrieve() // 해당 메서드, uri를 호출하면 응답값 받음
+                .bodyToMono() // 응답값을 mono로 받을 수 있다.
+    }
+
+}
---
+++ fc_class3/webflux1/src/main/java/com/example/webflux1/service/PostService.java
@@ -0,0 +1,9 @@
+package com.example.webflux1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    // webClient를 사용해서 mvc 기반의 server에 request함
+
+}
